462a87bf5fae61263b4c77ab09d5fb1ec10de331
MarcoAraujo16 <marcoaraujo1616@gmail.com>
2023-11-06 14:29:43 +0000

    Test

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ app.controller('postController', function($scope, $http) {
     $scope.posts = []; // Inicializa a variável que armazenará as postagens
 
     // Faz uma solicitação GET à API JSONPlaceholder para buscar postagens
-    $http.get('https://jsonplaceholder.typicode.com/posts')
+    $http.get('http://marcoaraujo16-001-site1.ftempurl.com/api/section/getall')
         .then(function(response) {
             $scope.posts = response.data; // Atribui os dados recebidos à variável de escopo $scope.posts
         });
